4e9d82af24d495ac41a9f5202098d4a2a843eacd
boomcs331 <tanapong.wa@tfac.or.th>
2025-10-13 18:55:34 +0700

    update route

--- config/config.php
+++ config/config.php
@@ -15,7 +15,7 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 
 // ตั้งค่า URL base
-define('BASE_URL', '/cps/');
+define('BASE_URL', '/cps-dev/');
 
 // ตั้งค่า path
 define('ROOT_PATH', __DIR__ . '/../');
